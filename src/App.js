8fbe309b0cb3bdbae50c4bcb42c60ba0003f0242
Matthew Brandon <mbrandon@pagerduty.com>
2018-08-03 15:12:08 -0700

    Small changes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ import { DateRange } from '@appbaseio/reactivesearch';
 class App extends Component {
   render() {
     return (
-      <section className="container">
-        <ReactiveBase
-          app="airbnb_clone_mbhdtech"
-          credentials="uWa0mgsKv:d8217a30-8340-4661-b0cc-10faf9da7a5f"
-          type="listing"
-        >
-          Hello from ReactiveSearch!
-        </ReactiveBase>
-
-        <DateRange
-          dataField="date_from"
-          componentId="DateRangeSensor"
-          title="When"
-          numberOfMonths={1}
-          queryFormat="basic_date" // yyyyMMdd
-        />
-      </section>
+          <ReactiveBase
+            app="airbnb_clone_mbhdtech"
+            credentials="uWa0mgsKv:d8217a30-8340-4661-b0cc-10faf9da7a5f"
+            type="listing"
+          >
+            Hello from ReactiveSearch!
+          </ReactiveBase>
     );
   }
 }
