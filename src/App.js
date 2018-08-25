245ab38f577c285fce8d794e033f45a877d0623d
mbhdtech <mbrandon@albany.edu>
2018-08-25 16:05:14 -0400

    Added range slider component along with the results section w/ css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { ReactiveBase } from '@appbaseio/reactivesearch';
 import { DateRange } from '@appbaseio/reactivesearch';
 import { NumberBox } from '@appbaseio/reactivesearch';
+import { RangeSlider } from '@appbaseio/reactivesearch';
+import { ResultCard } from '@appbaseio/reactivesearch';
 import './App.css';
 
 class App extends Component {
@@ -42,7 +44,58 @@ class App extends Component {
                         end: 16,
                     }}
             />
+
+            <RangeSlider
+                    componentId="PriceSensor"
+                    dataField="price"
+                    title="Price Range"
+                    range={{
+                        start: 10,
+                        end: 250,
+                    }}
+                    rangeLabels={{
+                        start: '$10',
+                        end: '$250',
+                    }}
+                    defaultSelected={{
+                        start: 10,
+                        end: 50,
+                    }}
+                    stepValue={10}
+                    interval={20}
+                    react={{
+                        and: ['DateRangeSensor'],
+                    }}
+                />
           </div>
+
+          <ResultCard
+                className="right-column"
+                componentId="SearchResult"
+                dataField="name"
+                size={12}
+                onData={data => ({
+                    image: data.image,
+                    title: data.name,
+                    description: (
+                        <div>
+                            <div className="price">${data.price}</div>
+                            <p className="info">{data.room_type} · {data.accommodates} guests</p>
+                        </div>
+                    ),
+                    url: data.listing_url,
+                })}
+                pagination
+                react={{
+                    and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+                }}
+                innerClass={{
+                    resultStats: 'result-stats',
+                    list: 'list',
+                    listItem: 'list-item',
+                    image: 'image',
+                }}
+            />
 
         </ReactiveBase>
       </div>
